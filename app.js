a523d6faaba1b7d6dea14badc965e8a2015699ce
handy <handy113@unist.ac.kr>
2019-05-12 20:30:33 +0900

    sending parameter to view

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-
+import { localsMiddleware } from "./middleware";
 const app = express();
 
 app.set("view engine", "pug");
@@ -29,7 +29,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 // app.use("/", globalRouter);
 // app.use("/users", userRouter);
